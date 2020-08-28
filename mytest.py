2272ac96b65f86e7f4d1e574ea0392eb86778c7d
xiaohuang001 <1147991352@qq.com>
2020-08-28 23:51:43 +0800

    1234

--- mytest.py
+++ mytest.py
@@ -1,9 +1,11 @@
 from unittest import TestSuite
 from unittest import TextTestRunner
 from mm.jhh import MyTest
-
+from HTMLTestRunner_cn import HTMLTestRunner
 suite=TestSuite()
 suite.addTest((MyTest('test_1')))
 
-TextTestRunner().run(suite)
+#TextTestRunner().run(suite)
 
+with open(r'C:\Users\Administrator\Desktop\aa\a.html','wb') as f:
+    HTMLTestRunner(stream=f, title='test', description='mytest').run(suite)
